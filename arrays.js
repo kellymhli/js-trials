88444b9a7b1231eac0708c1420418f0f469a91f3
Hackbright Student <no-reply@hackbrightacademy.com>
2019-10-24 12:36:56 -0700

    Add two array functions to arrays.js

--- arrays.js
+++ arrays.js
@@ -14,10 +14,24 @@ const printIndices = (items) => {
 /** 2. everyOtherItem */
 const everyOtherItem = (items) => {
 	// Replace this with your code
+
+  const result = []
+  for (const i of items) {
+    if (i % 2 === 0) {
+      result.push(i);
+    }
+  }
+  console.log(result)
 };
 
 
 /** 3. smallestNItems */
 const smallestNItems = (items, n) => {
 	// Replace this with your code
+
+  const sorted_items = items.sort((a, b) => a - b);
+  const sliced_items = sorted_items.slice(0, n);
+  sliced_items.reverse();
+
+  console.log(sliced_items);
 };
