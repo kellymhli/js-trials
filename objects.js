3ed8062ffb9e7dea299c5f0fbbf2d98af349f2e9
Hackbright Student <no-reply@hackbrightacademy.com>
2019-10-24 16:18:41 -0700

    Add getMelonAtPrice function

--- objects.js
+++ objects.js
@@ -24,4 +24,20 @@ function countWords(phrase) {
 /** 2. getMelonsAtPrice */
 function getMelonsAtPrice(price) {
 	// Replace this with your code
+  
+  const melon_prices = {
+    2.50: ['Cantaloupe', 'Honeydew'],
+    2.95: ['Watermelon'],
+    3.25: ['Musk', 'Crenshaw'],
+    14.25: ['Christmas']
+  };
+
+  if (price in melon_prices) {
+    console.log(price)
+    return (melon_prices[price].sort());
+  }
+  else {
+    return ;
+  }
+
 }
