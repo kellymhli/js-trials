5d00f0caae7774f9946b19ee978c5253eccad456
Hackbright Student <no-reply@hackbrightacademy.com>
2019-10-24 12:18:16 -0700

    Write printIndices function

---
+++ arrays.js
@@ -0,0 +1,23 @@
+"use strict";
+
+
+/** 1. printIndices */
+const printIndices = (items) => {
+	// Replace this with your code
+
+  for (const i in items) {
+    console.log(items[i], i);
+  }
+};
+
+
+/** 2. everyOtherItem */
+const everyOtherItem = (items) => {
+	// Replace this with your code
+};
+
+
+/** 3. smallestNItems */
+const smallestNItems = (items, n) => {
+	// Replace this with your code
+};
