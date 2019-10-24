a3119d7e10ac16e50ed46dc3583a9eabfd2343b6
Hackbright Student <no-reply@hackbrightacademy.com>
2019-10-24 13:01:35 -0700

    Write countWords function in objects.js

--- objects.js
+++ objects.js
@@ -4,6 +4,20 @@
 /** 1. countWords */
 function countWords(phrase) {
 	// Replace this with your code
+
+  const word_counts = {}
+
+  for (const word of phrase.split(' ')){
+    if (word_counts[word]){
+      word_counts[word] += 1;
+    }
+    else{
+      word_counts[word] = 1;
+    };
+  }
+
+  return word_counts;
+
 }
 
 
