e935f1c8642e5776aae8910773ab49a9eed889a3
Hackbright Student <no-reply@hackbrightacademy.com>
2019-10-24 12:51:25 -0700

    Write functions in functions.js

--- functions.js
+++ functions.js
@@ -5,12 +5,42 @@
 
 // Define your function here
 
+function hometown(town){
 
-** 2. getFullName */
+  return town === 'San Francisco';
+
+}
+
+function getFullName(fname, lname){
+
+  return `${fname} ${lname}`;
+}
 
 // Define your function here
 
 
 /** 3. calculateTotal */
+function calculateTotal(basePrice, state, tax=0.05) {
+  
+  let subtotal = basePrice * (1 + tax);
+  let fee = 0;
+  if (state === 'CA') {
+    fee = 0.03 * subtotal;
+  }
+  else if (state === 'PA') {
+    fee = 2;
+  }
+  else if (state === 'MA') {
+    if (basePrice <= 100){
+      fee = 1;
+    }
+    else {
+      fee = 3;
+    };
+  }
+
+  return subtotal + fee;
+
+}
 
 // Define your function here
