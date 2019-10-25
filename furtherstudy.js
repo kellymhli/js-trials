088b137da7af00630a3be60a599d63bdd750f5c5
Hackbright Student <no-reply@hackbrightacademy.com>
2019-10-24 17:42:29 -0700

    Start kidsGames function

--- furtherstudy.js
+++ furtherstudy.js
@@ -14,4 +14,44 @@ function wordsInCommon(words1, words2) {
   }
 
   return Array.from(result);
+
+};
+
+function kidsGame(names){
+  // Not complete but close
+  // Get first word and remove from consideration; add it to our output
+  
+    const output = [names.shift()];
+    console.log(output)
+    const firstLetterToWords = {};
+
+    for (const name of names) {
+      if (name[0] in firstLetterToWords) {
+        firstLetterToWords[name[0]].push(name);
+      }
+      else {
+        firstLetterToWords[name[0]] = [name];        
+      }
+    }
+
+    console.log(firstLetterToWords)
+    
+    while (true) {
+      const outputWord = output[output.length-1];
+      const startLetter = outputWord[outputWord.length-1];
+      console.log(outputWord, startLetter);
+      console.log(output);
+
+      if (!(startLetter in firstLetterToWords)){
+        console.log('We broke it');
+        break;
+      }
+
+      const word = firstLetterToWords[startLetter].shift();
+      output.push(word);
+
+    }
+
+    return output;
+
 };
